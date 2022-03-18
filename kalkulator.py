7a3f4827f6fc8598726044420ad21b4b87f01451
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-03-18 17:31:38 +0100

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+class kalkulator():
+    def __init__(self, wynik, dodawanie, odejmowanie, mnozenie): 
+        self.wynik = "="
+        self.dodawanie = "+"
+        self.odejmowanie = "-"
+        self.mnozenie = "*"
+
+    def zwracanie(self):
+        return "{} {} {} {}".format(self.wynik, self.dodawanie, self.odejmowanie, self.mnozenie)
+
+
+    def kalkulator():
+        pytanie = str(input(""))
+
+
+    
