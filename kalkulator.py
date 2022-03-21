28f50b2111fda9e42d155a028f90e09754a05eae
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-03-21 14:48:48 +0100

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,45 +1,66 @@
-def Kalkulator():
-    liczbznakow = 0
-    Pytanie1 = int(input("Ile liczb chcesz dodać/pomnożyć/podzielić/odjąć" + "\n"))
-    if Pytanie1 <= 0:
-        print("Liczba ta nie może być mniejsza lub równa 0!")
-        Kalkulator()
-    liczbznakow += Pytanie1
-    #print(liczbznakow)
-    Pytanie2 = str(input("Jakie działanie chcesz zrobić?(Dodawanie, odejmowanie, dzielenie, mnożenie)" + "\n"))
-    if Pytanie2 == "Dodawanie" or Pytanie2 == "dodawanie":
-        for x in range(liczbznakow):
-            PytanieDodawanie = input("Jakie liczby chcesz dodać?")
-            if PytanieDodawanie.isdigit() == False:
-                print("Słowo to nie liczba!")
-        return 
+import time
+from termcolor import colored, cprint
+def Dodawanie(x,y):
+    return x+y
+
+def Odejmowanie(x,y):
+    return x-y
+
+def Mnożenie(x,y):
+    return x*y
+
+def Dzielenie(x,y):
+    return x/y
+
+
+
+def Main():
+    Pytanie = str(input(
+'''Jakie chcesz zrobić działanie?
+Dodawanie
+Odejmowanie
+Mnożenie
+Dzielenie
+''' + "\n"))
+
+
+    match Pytanie:
+        case "Dodawanie" | "dodawanie":
+            liczb1 = float(input(colored("Wpisz pierwszą liczbę", 'red') + "\n"))
+            liczb2 = float(input(colored("Wpisz pierwszą liczbę", 'red') + "\n"))
+            wynik = Dodawanie(liczb1, liczb2)
+            zaaokraglenie = round(wynik)
+            print(f"Wynik to:{wynik} co w zaaokragleniu daje {zaaokraglenie}")
+            time.sleep(2)
+            Main()
+        case "Odejmowanie" | "odejmowanie":
+            liczb1 = float(input(colored("Wpisz pierwszą liczbę", 'cyan') + "\n"))
+            liczb2 = float(input(colored("Wpisz pierwszą liczbę", 'cyan') + "\n"))
+            wynik = Odejmowanie( liczb1, liczb2)
+            zaaokraglenie = round(wynik)
+            print(f"Wynik to:{wynik} co w zaaokragleniu daje {zaaokraglenie}")
+            time.sleep(2)
+            Main()
     
-    
-    if Pytanie2 == "Odejmowanie" or Pytanie2 == "odejmowanie":
-         for x in range(liczbznakow):
-             PytanieOdejmowanie = input("Jakie liczby chcesz odjac?")
-             if PytanieOdejmowanie.isdigit() == False:
-                 print("Słowo to nie liczba!")
-    
-    
-    if Pytanie2 == "Mnożenie" or Pytanie2 == "mnożenie":
-         for x in range(liczbznakow):
-             PytanieMnozenie = input("Jakie liczby chcesz pomnozyc?")
-             if PytanieMnozenie.isdigit() == False:
-                 print("Słowo to nie liczba!")
-                 
-                 
-    if Pytanie2 == "Dzielenie" or Pytanie2 == "dzielenie":
-          for x in range(liczbznakow):
-              PytanieDzielenie = input("Jakie liczby chcesz podzielic?")
-              if PytanieDzielenie.isdigit() == False:
-                  print("Słowo to nie liczba!")
+        case "Mnożenie" | "mnożenie":
+            liczb1 = float(input(colored("Wpisz pierwszą liczbę", 'blue') + "\n"))
+            liczb2 = float(input(colored("Wpisz pierwszą liczbę", 'blue') + "\n"))
+            wynik = Mnożenie(liczb1, liczb2)
+            zaaokraglenie = round(wynik)
+            print(f"Wynik to:{wynik} co w zaaokragleniu daje {zaaokraglenie}")
+            time.sleep(2)
+            Main()
+            
+        case "Dzielenie" | "dzielenie":
+            liczb1 = float(input(colored("Wpisz pierwszą liczbę", 'green') + "\n"))
+            liczb2 = float(input(colored("Wpisz pierwszą liczbę", 'green') + "\n"))
+            wynik = Dzielenie(liczb1, liczb2)
+            zaaokraglenie = round(wynik)
+            print(f"Wynik to:{wynik} co w zaaokragleniu daje {zaaokraglenie}")
+            time.sleep(2)
+            Main()
+            
         
-        
-        
-        
-        
-        
-        
-        
-Kalkulator()
+
+
+Main()
