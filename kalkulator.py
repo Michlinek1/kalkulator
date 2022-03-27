79e3b5d5309ebd5f0eeece54e92e65bce91c11aa
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-03-27 22:58:19 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,6 @@
 import time
 from termcolor import colored
+import math
 def Dodawanie(x,y):
     return x+y
 
@@ -12,7 +13,14 @@ def Mnożenie(x,y):
 def Dzielenie(x,y):
     return x/y
 
+def Potęga(x,y):
+    return x**y
 
+def Modulo(x,y):
+    return x%y
+
+def Logatytmy(x,y):
+    return math.log(x,y)
 
 def Main():
     Pytanie = str(input(
@@ -21,6 +29,9 @@ Dodawanie
 Odejmowanie
 Mnożenie
 Dzielenie
+Potęga
+Modulo
+Logatytmy
 ''' + "\n"))
 
 
@@ -59,10 +70,34 @@ Dzielenie
             print(f"Wynik to:{wynik} co w zaaokragleniu daje {zaaokraglenie}")
             time.sleep(2)
             Main()
-
+        case "Potęga" | "potęga":
+            liczb1 = float(input(colored("Wpisz podstawę potęgi", 'magenta') + "\n"))
+            liczb2 = float(input(colored("Wpisz wykładnik potęgi", 'magenta') + "\n"))
+            wynik = Potęga(liczb1,liczb2)
+            zaaokraglenie = round(wynik)
+            print(f"Wynik to:{wynik} co w zaaokragleniu daje {zaaokraglenie}")
+            time.sleep(2)
+            Main()
+            
+        case "Modulo" | "modulo":
+            liczb1 = float(input(colored("Wpisz pierwszą liczbę", 'magenta') + "\n"))
+            liczb2 = float(input(colored("Wpisz drugą liczbę", 'magenta') + "\n"))
+            wynik = Modulo(liczb1,liczb2)
+            zaaokraglenie = round(wynik)
+            if wynik == 0:
+                    print(f"Wynik to:{wynik}, ponieważ {liczb1} / {liczb2} nie ma żadnej reszty")
+            else:
+                    print(f"Wynik to:{wynik}, ponieważ {liczb1} / {liczb2} nie ma żadnej reszty")
+            time.sleep(2)
+            Main()
+        case "Logatytm" | "logatytm":
+            liczb1 = int((input(colored("Wpisz podstawę logatytmu", 'white') + "\n")))
+            liczb2 = int((input(colored("Wpisz liczbę logatytmowaną", 'white') + "\n")))
+            wynik = Logatytmy(liczb2, liczb1)
+            print(f"Wynik to:{wynik}")
+            time.sleep(2)
+            Main()
+        
 
 
 Main()
-            
-        
-
